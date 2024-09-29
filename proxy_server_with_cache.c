877ea0bb12e2e0f1a53d35fd7aba88623e8c4bfb
Ashwathama007 <111859913+vivaan01@users.noreply.github.com>
2024-09-30 00:57:25 +0530

    Create proxy_server_with_cache.c

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,17 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
