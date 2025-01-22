16550b838dfb7cf5173d5f0670ab3e363b2bf45f
Ashwathama007 <111859913+vivaan01@users.noreply.github.com>
2025-01-23 01:49:17 +0530

    Update proxy_server_without_cache.c
    comments added

--- proxy_server_without_cache.c
+++ proxy_server_without_cache.c
@@ -96,99 +96,132 @@ int cache_size=0;             //cache_size denotes the current size of the cache
 
 
 
-int sendErrorMessage(int socket, int status_code)
+/**
+ * Sends an HTTP error message to the client socket based on the status code.
+ * 
+ * @param socket       The socket descriptor for the client.
+ * @param status_code  The HTTP status code to send (e.g., 400, 403, etc.).
+ * @return 1 on success, -1 on failure.
+ */
+int sendErrorMessage(int socket, int status_code) {
+    char str[1024];             // Buffer to store the HTTP error response.
+    char currentTime[50];       // Buffer to store the current date and time in HTTP format.
+    time_t now = time(0);       // Get the current system time.
 
-{
+    // Convert the current time to the required HTTP date format (RFC 7231 format).
+    struct tm data = *gmtime(&now);  // Convert time to GMT (Coordinated Universal Time).
+    strftime(currentTime, sizeof(currentTime), "%a, %d %b %Y %H:%M:%S %Z", &data);
 
-	char str[1024];
+    // Generate the appropriate error response based on the HTTP status code.
+    switch (status_code) {
+        case 400:  // Bad Request
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Content-Length: 95\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n"
+                "<BODY><H1>400 Bad Request</H1>\n</BODY></HTML>",
+                currentTime
+            );
+            printf("400 Bad Request\n");  // Log the error type.
+            send(socket, str, strlen(str), 0);  // Send the error response to the client.
+            break;
 
-	char currentTime[50];
+        case 403:  // Forbidden
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 403 Forbidden\r\n"
+                "Content-Length: 112\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n"
+                "<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>",
+                currentTime
+            );
+            printf("403 Forbidden\n");
+            send(socket, str, strlen(str), 0);  // Send the error response.
+            break;
 
-	time_t now = time(0);
+        case 404:  // Not Found
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 404 Not Found\r\n"
+                "Content-Length: 91\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n"
+                "<BODY><H1>404 Not Found</H1>\n</BODY></HTML>",
+                currentTime
+            );
+            printf("404 Not Found\n");
+            send(socket, str, strlen(str), 0);
+            break;
 
+        case 500:  // Internal Server Error
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 500 Internal Server Error\r\n"
+                "Content-Length: 115\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n"
+                "<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>",
+                currentTime
+            );
+            // printf("500 Internal Server Error\n");
+            send(socket, str, strlen(str), 0);
+            break;
 
+        case 501:  // Not Implemented
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 501 Not Implemented\r\n"
+                "Content-Length: 103\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>501 Not Implemented</TITLE></HEAD>\n"
+                "<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>",
+                currentTime
+            );
+            printf("501 Not Implemented\n");
+            send(socket, str, strlen(str), 0);
+            break;
 
-	struct tm data = *gmtime(&now);
+        case 505:  // HTTP Version Not Supported
+            snprintf(
+                str, sizeof(str),
+                "HTTP/1.1 505 HTTP Version Not Supported\r\n"
+                "Content-Length: 125\r\n"
+                "Connection: keep-alive\r\n"
+                "Content-Type: text/html\r\n"
+                "Date: %s\r\n"
+                "Server: VaibhavN/14785\r\n\r\n"
+                "<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n"
+                "<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>",
+                currentTime
+            );
+            printf("505 HTTP Version Not Supported\n");
+            send(socket, str, strlen(str), 0);
+            break;
 
-	strftime(currentTime,sizeof(currentTime),"%a, %d %b %Y %H:%M:%S %Z", &data);
+        default:  // Unsupported or unknown status code
+            return -1;  // Return -1 to indicate failure.
+    }
 
-
-
-	switch(status_code)
-
-	{
-
-		case 400: snprintf(str, sizeof(str), "HTTP/1.1 400 Bad Request\r\nContent-Length: 95\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n<BODY><H1>400 Bad Rqeuest</H1>\n</BODY></HTML>", currentTime);
-
-				  printf("400 Bad Request\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		case 403: snprintf(str, sizeof(str), "HTTP/1.1 403 Forbidden\r\nContent-Length: 112\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>", currentTime);
-
-				  printf("403 Forbidden\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		case 404: snprintf(str, sizeof(str), "HTTP/1.1 404 Not Found\r\nContent-Length: 91\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n<BODY><H1>404 Not Found</H1>\n</BODY></HTML>", currentTime);
-
-				  printf("404 Not Found\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		case 500: snprintf(str, sizeof(str), "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 115\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>", currentTime);
-
-				  //printf("500 Internal Server Error\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		case 501: snprintf(str, sizeof(str), "HTTP/1.1 501 Not Implemented\r\nContent-Length: 103\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Implemented</TITLE></HEAD>\n<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>", currentTime);
-
-				  printf("501 Not Implemented\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		case 505: snprintf(str, sizeof(str), "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 125\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>", currentTime);
-
-				  printf("505 HTTP Version Not Supported\n");
-
-				  send(socket, str, strlen(str), 0);
-
-				  break;
-
-
-
-		default:  return -1;
-
-
-
-	}
-
-	return 1;
-
+    return 1;  // Return 1 to indicate success.
 }
-
-
 
 int connectRemoteServer(char* host_addr, int port_num)
 
